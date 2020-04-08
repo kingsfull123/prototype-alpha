28b3f05821ca0eb078efb4386685a91d7decb568
jason yang <syan19@live.cn>
2020-04-08 16:29:32 +1000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const postCssPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/index.js',
+    output: {
+        path: path.resolve(__dirname, 'app'),
+        filename: 'bundled.js'
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css/i,
+                use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {plugins: postCssPlugins}}]
+            }
+        ]
+    }
+}
